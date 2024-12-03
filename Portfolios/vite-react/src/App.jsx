556e830e57cf1dfd9e6ce4221d5b854b10b9636e
YungBulgaria <mickenev@gmail.com>
2024-12-02 19:59:56 -0600

    final commit

--- Portfolios/vite-react/src/App.jsx
+++ Portfolios/vite-react/src/App.jsx
@@ -1,8 +1,34 @@
 import {Canvas} from "@react-three/fiber";
+import {OrbitControls} from "@react-three/drei";
+import {useRef} from "react";
+
+const RotatingCube = () => {
+  const meshRef = useRef();
+
+  useFrame(() => {
+    if(meshRef.current) {
+      meshRef.current.rotation.y += 0.01
+      meshRef.current.rotation.x += 0.01
+    }
+  })
+
+  return (
+    <mesh ref={meshRef}>
+        <cylinderGeometry args={[1, 1, 1, 32]} />
+        <meshLambertMaterial color="#468585" emissive="#468585" />
+    </mesh>
+  )
+}
 const App = () => {
   return (
-    <Canvas>
-      Hello Three.js
+    <Canvas style={{height: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+      <OrbitControls enableZoom={true} enablePan enableRotate />
+
+      <directionalLight position={[1, 1, 1]} intensity={10} color={0x9CDBA6} />
+
+      <color attatch="background" args={['#F0F0F0']} />
+
+      <RotatingCube />
     </Canvas>
   )
 }
